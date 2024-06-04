97970ee31a0010249d9e9be3eae6f1e409d16692
Serega1067 <serega1067@yandex.ru>
2024-06-04 23:58:52 +0700

    Add class Holder

--- Main.kt
+++ Main.kt
@@ -1,3 +1,24 @@
+interface ValueChangeListener {
+    fun onNewValue(num: Int)
+}
+
+public final class Holder private constructor(number: Int) {
+    var number: Int = number
+        set(value) {
+            listener?.onNewValue(value)
+            field = value
+        }
+
+    var listener: ValueChangeListener? = null
+
+    companion object {
+        const val DEFAUlT_NUMBER = -1
+        fun createHolder(defaultNumber: Int): Holder {
+            return Holder(defaultNumber)
+        }
+    }
+}
+
 fun main() {
     val holder = Holder.createHolder(Holder.DEFAULT_NUMBER)
     holder.listener = object : ValueChangeListener {

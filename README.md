0c808446e5e78b5a9ae780ac933c18884ebe9a18
Serega1067 <serega1067@yandex.ru>
2024-06-07 21:53:17 +0700

    Moved the Main file to the Task1 folder and modified Readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Class practice at Kotlin.
+
 ## Task 1. Add classes.
 Write an implementation of the Holder class and interface  
 valueChangeListener, such that the program compiles,  
 and when executing the main function, "New value is 1" is displayed  
 on the screen.
+
+## Task 2. Class hierarchy.
+To build a hierarchy of classes in such a way that after executing  
+the main function, the correct result of calculating 3 formulas is output.
+
---
+++ Task1/Main.kt
@@ -0,0 +1,30 @@
+interface ValueChangeListener {
+    fun onNewValue(number: Int)
+}
+
+public final class Holder private constructor(number: Int) {
+    var number: Int = number
+        set(value) {
+            listener?.onNewValue(value)
+            field = value
+        }
+
+    var listener: ValueChangeListener? = null
+
+    companion object {
+        const val DEFAULT_NUMBER = -1
+        fun createHolder(defaultNumber: Int): Holder {
+            return Holder(defaultNumber)
+        }
+    }
+}
+
+fun main() {
+    val holder = Holder.createHolder(Holder.DEFAULT_NUMBER)
+    holder.listener = object : ValueChangeListener {
+        override fun onNewValue(number: Int) {
+            println("New value is $number")
+        }
+    }
+    holder.number = 1
+}
